86c2fba5d6808779698351e6b79a0e2d8e07ed28
wdinizsp <extern.wilson.silva@volkswagen.com.br>
2021-03-09 11:31:15 -0300

    Novo Exercicio 09.03.2021 113000

--- entities/Worker.java
+++ entities/Worker.java
@@ -7,16 +7,15 @@ import java.util.List;
 import entities.enums.WorkerLevel;
 
 public class Worker {
-	
+
 	private String name;
 	private WorkerLevel level;
 	private Double baseSalary;
-	
+
 	private Department department;
 	private List<HourContract> contracts = new ArrayList<>();
-	
+
 	public Worker() {
-		
 	}
 
 	public Worker(String name, WorkerLevel level, Double baseSalary, Department department) {
@@ -65,24 +64,22 @@ public class Worker {
 	public void addContract(HourContract contract) {
 		contracts.add(contract);
 	}
-	
+
 	public void removeContract(HourContract contract) {
 		contracts.remove(contract);
 	}
-	
+
 	public double income(int year, int month) {
 		double sum = baseSalary;
 		Calendar cal = Calendar.getInstance();
-		for (HourContract c: contracts) {
+		for (HourContract c : contracts) {
 			cal.setTime(c.getDate());
 			int c_year = cal.get(Calendar.YEAR);
 			int c_month = 1 + cal.get(Calendar.MONTH);
 			if (year == c_year && month == c_month) {
 				sum += c.totalValue();
 			}
-			
 		}
 		return sum;
 	}
-
 }
