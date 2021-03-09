f8482ce2fd90150b08320a6942c5155d70d5b2b5
wdinizsp <extern.wilson.silva@volkswagen.com.br>
2021-03-09 16:09:43 -0300

    Novos Exercicios 09.03.2021 160800

--- application/Program_19.java
+++ application/Program_19.java
@@ -20,9 +20,9 @@ public class Program_19 {
 		
 		SimpleDateFormat sdf = new SimpleDateFormat ("dd/MM/yyyy");
 		
-		System.out.println("Enter departmetn's name: ");
+		System.out.print("Enter department's name: ");
 		String departmentName = sc.nextLine();
-		System.out.print("Enter worker data: ");
+		System.out.println("Enter worker data: ");
 		System.out.print("Name: ");
 		String workerName  = sc.nextLine();
 		System.out.print("Level: ");
@@ -35,7 +35,7 @@ public class Program_19 {
 		System.out.print("How many contracts to this worker? ");
 		int n = sc.nextInt();
 		
-		for (int i=1; i<n; i++) {
+		for (int i=1; i<=n; i++) {
 			System.out.println("Enter contract #" + i + " data:");
 			System.out.print("Date (DD/MM/YYYY):  ");
 			Date contractDate = sdf.parse(sc.next());
@@ -47,6 +47,14 @@ public class Program_19 {
 			worker.addContract(contract);
 		}
 		
+		System.out.println();
+		System.out.print("Enter month and year to calculate income (MM/YYYY): ");
+		String monthAndYear = sc.next();
+		int month = Integer.parseInt(monthAndYear.substring(0, 2));
+		int year = Integer.parseInt(monthAndYear.substring(3));
+		System.out.println("Name: " + worker.getName());
+		System.out.println("Department: " + worker.getDepartment().getName());
+		System.out.println("Income for " + monthAndYear + ": " + String.format("%.2f", worker.income(year, month)));
 		
 		sc.close();
 
